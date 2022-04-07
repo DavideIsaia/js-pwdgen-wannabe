c6d1fcc691d9983c66a92647bfeafe35ffa33945
DavideIsaia <davide.enjoy@hotmail.it>
2022-04-07 09:50:38 +0200

    modificata const Name in myName

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 // Chiedo all'utente nome, cognome e colore per salvarli nelle variabili corrispondenti e mando un messaggio sulla console di debug per vedere che tutto funzioni correttamente
-const Name = prompt('Inserisci il tuo nome');
-console.log(Name);
+const myName = prompt('Inserisci il tuo nome');
+console.log(myName);
 
 const lastName = prompt('Inserisci il tuo cognome');
 console.log(lastName);
@@ -11,7 +11,7 @@ console.log(prefColor);
 
 // metto insieme le stringhe precedenti per creare la password
 const message = `La password creata è: 
-${Name}${lastName}${prefColor}22`;
+${myName}${lastName}${prefColor}22`;
 console.log(message);
 
 // faccio vedere all'utente il risultato
